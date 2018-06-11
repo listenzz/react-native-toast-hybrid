60e435e0c9ba2da2d623668c5dffbf48d4da5a6c
Listen <listenzz@163.com>
2018-06-11 17:01:02 +0800

    refactor ios playground

---
+++ playground/ios/HUDPlayground/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HUDPlayground
+//
+//  Created by Listen on 2018/6/11.
+//  Copyright © 2018年 Listen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
--- playground/ios/Pods/Target Support Files/Pods-playground/Pods-playground-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_playground : NSObject
-@end
-@implementation PodsDummy_Pods_playground
-@end
