059c1034b928d288bff06f76f4e29daeeb15c781
Listen <listenzz@163.com>
2018-08-02 20:33:22 +0800

    optimize code

--- playground/src/App.js
+++ playground/src/App.js
@@ -106,33 +106,3 @@ export default class App extends Component {
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'flex-start',
-    alignItems: 'stretch',
-    backgroundColor: '#F5FCFF',
-    paddingTop: 16 + 88,
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-  button: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    height: 40,
-  },
-
-  buttonText: {
-    backgroundColor: 'transparent',
-    color: 'rgb(34,88,220)',
-  },
-});
